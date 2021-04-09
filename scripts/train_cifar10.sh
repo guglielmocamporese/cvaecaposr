e3d9f9afe82817d65b3afe5d0484d60a0384e30a
guglielmocamporese <guglielmocamporese@gmail.com>
2021-04-09 10:46:52 +0200

    added train and test scripts

---
+++ scripts/train_cifar10.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+python main.py \
+    --data_base_path "./data" \
+    --dataset "cifar10" \
+    --val_ratio 0.2 \
+    --seed 1234 \
+    --batch_size 32 \
+    --split_num 0 \
+    --z_dim 128 \
+    --lr 5e-5 \
+    --t_mu_shift 10.0 \
+    --t_var_scale 1.0 \
+    --alpha 1.0 \
+    --beta 0.01 \
+    --margin 10.0 \
+    --in_dim_caps 16 \
+    --out_dim_caps 32 \
+    --checkpoint "" \
+    --epochs 100 \
+    --mode "train"
